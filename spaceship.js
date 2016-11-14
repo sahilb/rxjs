8a029f02ac93177a469aba97c434616c89df260a
Sahil Budhiraja <sbudhiraja@netflix.com>
2016-11-14 08:56:33 -0800

    spaceship, super enemy

--- spaceship.js
+++ spaceship.js
@@ -2,77 +2,97 @@ var log = console.log.bind(console);
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-var Stars = createEntities(canvas.width, canvas.height, 100, 4);
-var Enemies = createEntities(canvas.width, canvas.height, 5, 12);
+var Stars 	= createEntities(200, 4, '#F1F1F1');
+var Enemies = createEntities(15, 12, '#FF6666');
+var SuperEnemies = createEntities(1, 20, '#FF0000');
+var SpaceShip = newEntity(12, '#00FF00');
+SpaceShip.x = canvas.width/2;
+SpaceShip.y = canvas.height - 40;
 
-function createEntities(magnitudeX, magnitudeY, count, size) {
+function newEntity(size, color){
+	return {
+		x: ((Math.random() * 10000) | 0) % (canvas.width - size - 5),
+		y: ((Math.random() * 10000) | 0) % (canvas.height - size - 5),
+		size: size > 10 ? size : (1 + (Math.random() * size)),
+		color: color
+	}
+}
+function createEntities(count, size, color) {
     var stars;
     Rx.Observable.range(0, count)
         .toArray()
         .subscribe(arr => {
-            stars = arr.map(x => {
-                return {
-                    x: ((Math.random() * 10000) | 0) % (magnitudeX - size),
-                    y: ((Math.random() * 10000) | 0) % (magnitudeY - size),
-                    size: size > 10 ? size : (1 + (Math.random() * size))
-                };
-            });
+            stars = arr.map( () => newEntity(size, color));
         });
-    return stars
+    return stars;
 }
 
 function drawTriangle(x, y, width, color, direction) {
     ctx.fillStyle = color;
     ctx.beginPath();
     ctx.moveTo(x - width, y);
-    ctx.lineTo(x, direction === 'up'? y - width : y + width);
+    ctx.lineTo(x, direction === 'up' ? y - width : y + width);
     ctx.lineTo(x + width, y);
     ctx.lineTo(x - width, y);
     ctx.fill();
 }
 
-function render() {
-	log('rendering');
-    ctx.fillStyle = "#111111";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    
-    Enemies.forEach(enemy => {
-        drawTriangle(enemy.x, enemy.y, enemy.size, '#FF0000', 'down');
-    });
-    ctx.fillStyle = "#F1F1F1";
-    Stars.forEach(star => {
-        ctx.fillRect(star.x, star.y, star.size, star.size);
-    });
+function createStream(entities, displacement, interval) {
+    return Rx.Observable
+        .interval(interval)
+        .flatMap(() => {
+            newEntities = entities.map(entity => {
+                if (entity.y + displacement + entity.size >= canvas.height) {
+                    entity.y = entity.size;
+                    entity.x = ((Math.random() * 10000) | 0) % (canvas.width - entity.size);
+                } else {
+                    entity.y += displacement;
+                }
+                return entity;
+            });
+            return Rx.Observable.of(newEntities);
+        })
 }
 
-var stars = Rx.Observable.interval(1500).map(() => {
-	log('stars ')
-    Stars.forEach((star) => {
-        if (star.y + 20 >= canvas.height) {
-            star.y = 5;
-            star.x = ((Math.random() * 10000) | 0) % (canvas.width - 10);
-        } else {
-            star.y += 15;
-        }
+var stars 			= createStream(Stars, 5, 1500);
+var enemies 		= createStream(Enemies, 15, 800);
+var superEnemies 	= createStream(SuperEnemies, 30, 200);
+var spaceShips  	= Rx.Observable.of(SpaceShip).merge(Rx.Observable.fromEvent(document, 'keydown'))
+	.map((ev)=> (ev.color ||  ev.key === 'ArrowRight' ? 10 : (ev.key === 'ArrowLeft' ? -10 : 0) ))
+	.filter(delta => delta !== 0)
+	.flatMap(delta => {
+		if( SpaceShip.x + delta + 30 > canvas.width){
+			// do nothing, crossing right
+		}else if (SpaceShip.x + delta - 30 < 0 ){
+			// do nothing, crossing left
+		}else {
+			SpaceShip.x += delta;
+		}
+		return Rx.Observable.of(SpaceShip);
+	});
+
+
+var rendering = Rx.Observable.combineLatest(stars, enemies, superEnemies, spaceShips).subscribe(render);
+
+function render(...args) {
+    let [st, en, superEn, spaceShip] = args[0];
+    ctx.fillStyle = "#111111";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+    st.forEach(star => {
+        ctx.fillStyle = star.color;
+        ctx.fillRect(star.x, star.y, star.size, star.size);
     });
-});
 
-var enemies = Rx.Observable.interval(700).map(() => {
-	log('enemies');
-    Enemies.forEach(enemy => {
-        if (enemy.y + 50 >= canvas.height) {
-            enemy.y = 20;
-            enemy.x = ((Math.random() * 10000) | 0) % (canvas.width - enemy.size);
-        }
-        enemy.y = enemy.y + 10;
-    })
-});
+    en.forEach(entity => {
+        drawTriangle(entity.x, entity.y, entity.size, entity.color, 'down');
+    });
 
-Rx.Observable.merge(stars, enemies).subscribe(render);
+    superEn.forEach(entity => {
+        drawTriangle(entity.x, entity.y, entity.size, entity.color, 'down');
+    });
 
+	drawTriangle(spaceShip.x, spaceShip.y, spaceShip.size, spaceShip.color, 'up');
 
-
-// star stream that gives a series of stars
-// spaceship stream that gives spaceship and position
-// enemies stream that gives enemies positions
+}
